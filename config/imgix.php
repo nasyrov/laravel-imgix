e6b95f2330ec5e72d51b501ed32adef377aeb024
Evgenii Nasyrov <inasyrov@yandex.ru>
2017-06-09 14:38:49 +0100

    Reorder items in the config

--- config/imgix.php
+++ config/imgix.php
@@ -4,23 +4,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Sharding
-    |--------------------------------------------------------------------------
-    |
-    | Domain sharding enables you to spread image requests across multiple
-    | domains. This allows you to bypass the requests-per-host limits of
-    | browsers.
-    |
-    | Available Settings:
-    | - \Imgix\ShardStrategy::CRC
-    | - \Imgix\ShardStrategy::CYCLE
-    |
-    */
-
-    'sharding' => \Imgix\ShardStrategy::CRC,
-
-    /*
-    |--------------------------------------------------------------------------
     | Domains
     |--------------------------------------------------------------------------
     |
@@ -34,9 +17,10 @@ return [
         // 'cdn-2.imgix.net'
     ],
 
+
     /*
     |--------------------------------------------------------------------------
-    | HTTPS
+    | Use Https
     |--------------------------------------------------------------------------
     |
     */
@@ -45,7 +29,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Signed URLs
+    | Sign Key
     |--------------------------------------------------------------------------
     |
     | To produce a signed URL, you must enable secure URLs on your source
@@ -54,5 +38,22 @@ return [
     */
 
     'signKey' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sharding Strategy
+    |--------------------------------------------------------------------------
+    |
+    | Domain sharding enables you to spread image requests across multiple
+    | domains. This allows you to bypass the requests-per-host limits of
+    | browsers.
+    |
+    | Available settings:
+    | - \Imgix\ShardStrategy::CRC
+    | - \Imgix\ShardStrategy::CYCLE
+    |
+    */
+
+    'shardStrategy' => \Imgix\ShardStrategy::CRC,
 
 ];
