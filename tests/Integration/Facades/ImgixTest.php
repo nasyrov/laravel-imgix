25261f68b0858df9f03b0c2a589b800442626dd6
Nasyrov Evgenii <inasyrov@ya.ru>
2017-06-10 12:37:13 +0100

    Update imgix facade test

--- tests/Integration/Facades/ImgixTest.php
+++ tests/Integration/Facades/ImgixTest.php
@@ -2,7 +2,8 @@
 
 namespace Nasyrov\Laravel\Imgix\Tests\Unit\Facades;
 
-use Nasyrov\Laravel\Imgix\Facades\Imgix;
+use Nasyrov\Laravel\Imgix\Facades\Imgix as ImgixFacade;
+use Nasyrov\Laravel\Imgix\Imgix;
 use Nasyrov\Laravel\Imgix\ImgixServiceProvider;
 use Nasyrov\Laravel\Imgix\Tests\Integration\TestCase;
 
@@ -11,7 +12,7 @@ class ImgixTest extends TestCase
     /** @test */
     public function it_can_initialise()
     {
-        $this->assertInstanceOf('Nasyrov\Laravel\Imgix\Imgix', Imgix::getFacadeRoot());
-        $this->assertSame($this->app[ImgixServiceProvider::ALIAS], Imgix::getFacadeRoot());
+        $this->assertInstanceOf(Imgix::class, ImgixFacade::getFacadeRoot());
+        $this->assertSame($this->app[ImgixServiceProvider::ALIAS], ImgixFacade::getFacadeRoot());
     }
 }
