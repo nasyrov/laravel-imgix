81cb2880751202ac49eafa380033bd1a088c8b40
Nasyrov Evgenii <inasyrov@ya.ru>
2017-06-11 21:00:26 +0100

    Rename facade test case and simplify it

--- tests/Integration/Facades/ImgixTest.php
+++ tests/Integration/Facades/ImgixTest.php
@@ -4,15 +4,13 @@ namespace Nasyrov\Laravel\Imgix\Tests\Unit\Facades;
 
 use Nasyrov\Laravel\Imgix\Facades\Imgix as ImgixFacade;
 use Nasyrov\Laravel\Imgix\Imgix;
-use Nasyrov\Laravel\Imgix\ImgixServiceProvider;
 use Nasyrov\Laravel\Imgix\Tests\Integration\TestCase;
 
 class ImgixTest extends TestCase
 {
     /** @test */
-    public function it_can_initialise()
+    public function it_provides_facade()
     {
         $this->assertInstanceOf(Imgix::class, ImgixFacade::getFacadeRoot());
-        $this->assertSame($this->app[ImgixServiceProvider::ALIAS], ImgixFacade::getFacadeRoot());
     }
 }
