6aff6baba965ec444ba5940d9451519dafccbba7
Nasyrov Evgenii <inasyrov@ya.ru>
2017-06-11 20:58:05 +0100

    Add imgix service provider tests

--- tests/Integration/ImgixServiceProviderTest.php
+++ tests/Integration/ImgixServiceProviderTest.php
@@ -2,6 +2,10 @@
 
 namespace Nasyrov\Laravel\Imgix\Tests\Integration;
 
+use Imgix\UrlBuilder;
+use Nasyrov\Laravel\Imgix\Imgix;
+use Nasyrov\Laravel\Imgix\ImgixServiceProvider;
+
 class ImgixServiceProviderTest extends TestCase
 {
     /** @test */
@@ -9,5 +13,16 @@ class ImgixServiceProviderTest extends TestCase
     {
         $config = include dirname(__DIR__) . '/../config/imgix.php';
         $this->assertEquals($config, $this->app['config']['imgix']);
+    }
+
+    /** @test */
+    public function it_registers()
+    {
+        $this->assertTrue($this->app->bound(UrlBuilder::class));
+        $this->assertInstanceOf(UrlBuilder::class, $this->app->make(UrlBuilder::class));
+
+        $this->assertTrue($this->app->bound(Imgix::class));
+        $this->assertInstanceOf(Imgix::class, $this->app->make(Imgix::class));
+        $this->assertInstanceOf(Imgix::class, $this->app->make(ImgixServiceProvider::ALIAS));
     }
 }
